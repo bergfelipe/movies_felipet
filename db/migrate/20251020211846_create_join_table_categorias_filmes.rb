97530efbe70835f73b98b1201fa8671e785db262
04248685205 <felipe.bosso@sefin.ro.gov.br>
2025-10-21 07:21:16 -0400

    fix: remove migration duplicada de categorias_filmes

--- db/migrate/20251020211846_create_join_table_categorias_filmes.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableCategoriasFilmes < ActiveRecord::Migration[7.1]
-  def change
-    create_join_table :categorias, :filmes do |t|
-      # t.index [:categoria_id, :filme_id]
-      # t.index [:filme_id, :categoria_id]
-    end
-  end
-end
