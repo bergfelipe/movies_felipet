570a55e742915ace40c84de844f623126cc6828f
Felipe Bernardo <95366231+bergfelipe@users.noreply.github.com>
2025-10-24 11:20:04 -0400

    debugando

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,20 +1,31 @@
-PasswordsControllerclass PasswordsController < Devise::PasswordsController
+class PasswordsController < Devise::PasswordsController
   def create
     self.resource = resource_class.find_by(email: resource_params[:email])
+
     if resource
+      # Cria um token de redefinição de senha
       token = resource.send(:set_reset_password_token)
       url = edit_password_url(resource, reset_password_token: token, locale: I18n.locale)
 
-      html = ApplicationController.render(
-        html: "<p>Para redefinir sua senha, clique <a href='#{url}'>aqui</a>.</p>"
-      )
+      # Monta o HTML do e-mail
+      html = <<-HTML
+        <p>Olá #{resource.name || 'usuário'},</p>
+        <p>Você solicitou a redefinição da sua senha. Para continuar, clique no link abaixo:</p>
+        <p><a href="#{url}">Redefinir minha senha</a></p>
+        <p>Se você não fez essa solicitação, ignore este e-mail.</p>
+        <br>
+        <p>Atenciosamente,<br>Equipe Movies 🎬</p>
+      HTML
 
+      # Envia via SendGrid
       SendgridApiMailer.send_email(
         to: resource.email,
         subject: "Redefinição de senha - Movies",
         content: html
       )
     end
-    redirect_to new_session_path(resource_name), notice: "Se o e-mail existir, o link foi enviado."
+
+    redirect_to new_session_path(resource_name),
+                notice: "Se o e-mail existir, um link de redefinição foi enviado."
   end
 end
